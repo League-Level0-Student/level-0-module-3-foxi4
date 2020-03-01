b57d1043827fb46f34b2d6c001ab8c3cf54aeadd
League Student <league@iMac-20.attlocal.net>
2020-03-01 14:27:37 -0800

    3/1/20
    hooray i did really well today

--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -12,13 +12,19 @@ public class CrazyCatLady {
 		// 1. Ask the user how many cats they have
 String cats=JOptionPane.showInputDialog("How many cats do you own?");
 		// 2. Convert their answer into an int
-
+int cat = Integer.parseInt(cats);	
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+if(cat>=3) {
+ JOptionPane.showMessageDialog(null, "You are a crazy cat lady. There's no denying it.");}
 
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+if(cat<3&&cat>0) {
+	playVideo("https://www.youtube.com/watch?v=5fCM0Ttx0WA");
+}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+		if(cat==0) {
+			playVideo("https://www.youtube.com/watch?v=oj_yLBltPE8");
+		}
 	}
 
 	static void playVideo(String videoURL) {
---
+++ src/_04_are_you_happy/AreYouHappy.java
@@ -0,0 +1,12 @@
+package _04_are_you_happy;
+
+import javax.swing.JOptionPane;
+
+public class AreYouHappy {
+public static void main(String[] args) {
+	String answer= JOptionPane.showInputDialog("Are you happy?");
+	if(answer==no) {
+		
+	}
+}
+}

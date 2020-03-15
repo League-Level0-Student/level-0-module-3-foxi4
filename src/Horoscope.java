d5442c840bd5dde6720e31c768a78a073cebaefe
root <root@d82f63fe42bf>
2020-03-15 21:31:31 +0000

    3/15/20

--- src/Horoscope.java
+++ src/Horoscope.java
@@ -2,14 +2,72 @@ import javax.swing.JOptionPane;
 
 public class Horoscope {
 public static void main(String[] args) {
-	String answer= JOptionPane.showInputDialog("When were you born? (Ex. 11, which means January 1st)");
-	int answers= Integer.parseInt(answer);
-	if(answers<218&&>120) {
-		JOptionPane.showMessageDialog(null, "Congrats! You are an aquarius!");
+	String answer= JOptionPane.showInputDialog("What is your star sign?");
+	
+	if(answer.equals("Aries")) { 
+		
+		JOptionPane.showMessageDialog(null, "You are couragous and energetic. You love a challenge and are a good leader.");
 	}
+	if(answer.equals("Taurus")) {
+		JOptionPane.showMessageDialog(null, "You are a very deliberate thinker. You love to have a good laugh and spend time with family.");
+	}
+	if(answer.equals("Gemini")) {
+		JOptionPane.show
+	}
+	if(answer.equals("Cancer")) {
+		
+	}
+	if(answer.equals("Leo")) {
+		
+	}
+	if(answer.equals("Virgo")) {
+		
+	}
+	if(answer.equals("Libra")) {
+		
+	}
+	if(answer.equals("Scorpio")) {
+		
+	}
+	if(answer.equals("Sagittarius")) {
+		
+	}
+	if(answer.equals("Capricorn")) {
+		
+	}
+	if(answer.equals("Aquarius")) {
+		
+	}
+	if(answer.equals("Pisces")) {
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	
-	
-	
-
 }
 }

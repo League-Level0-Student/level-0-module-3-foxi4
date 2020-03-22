a0f32108171ce85c894070c68294c89d2a8b91d7
root <root@fc025354d907>
2020-03-22 21:40:00 +0000

    HIHIHIHIHIHIHIHIH

--- src/_06_birthday_reminder/BirthdayReminder.java
+++ src/_06_birthday_reminder/BirthdayReminder.java
@@ -26,10 +26,22 @@ public class BirthdayReminder {
 			System.out.print(momsBirthday);
 		}
 		// 5. if user asked for "dad"
+		
 			// print dad's birthday
+		if(birthday.equals("dad")) {
+			System.out.print(dadsBirthday);
+		}
 		// 6. if user asked for your name
 			// print myBirthday
+		if(birthday.equals("Lauren")) {
+			System.out.print(myBirthday);
+		}
+		if(birthday.contentEquals("Kevin")) {
+			System.out.print(brosBirthday);
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+	} else {
+	JOptionPane.showMessageDialog(null, "Sorry, I don't remember that person's birthday.");
+			
+		}
 	} 
 }
